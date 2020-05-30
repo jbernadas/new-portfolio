f8223c1d3912bfc0b2290725f361a543b23adc85
Joseph Bernadas <jb3rns@gmail.com>
2020-05-30 00:02:45 -0700

    Removed home navbar button on inner-pages, kept on home page but disappears on scroll down

--- js/includes/scripts.js
+++ js/includes/scripts.js
@@ -16,10 +16,12 @@ $(document).ready(function(){
     if($(window).scrollTop() > 440) {
       $("#nav-brand-name").removeClass("d-none");
       $("#navbar").addClass("col-lg-10 col-xl-11");
+      $("#navbar > .navbar-nav > .nav-item:first-child").addClass("d-none");
     }
     else {
       $('#nav-brand-name').addClass('d-none');
       $("#navbar").removeClass("col-lg-10 col-xl-11");
+      $("#navbar > .navbar-nav > .nav-item:first-child").removeClass("d-none");
     }
   }
 
